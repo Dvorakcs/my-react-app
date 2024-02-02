7aa9d2b9b05941fb4a267a202aadb022db53072c
Dvôrakcs <Mhsf15@hotmail.com>
2024-02-02 16:28:41 -0300

    use state class

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Garage from './Garage';
 import FGarage from './FGarage';
-
+import CreaterCar from './CreateCar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Garage/>);
+root.render(<CreaterCar/>);
 // or root.render(<FCar/>);
